854c9204a3e4fd6f0a9748cb312f1d6b507b204d
ricardo.pleite <ricardo.pleite@CAS511933W0713.senacsp.edu.br>
2017-11-09 22:01:13 -0200

    Melhorias validador

--- housebay/src/main/java/br/senac/tads/housebay/controller/ValidateCliente.java
+++ housebay/src/main/java/br/senac/tads/housebay/controller/ValidateCliente.java
@@ -9,8 +9,11 @@ import br.senac.tads.housebay.exception.ClienteException;
 import br.senac.tads.housebay.exception.ClienteException;
 import br.senac.tads.housebay.model.Cliente;
 import br.senac.tads.housebay.model.Cliente;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
+import sun.util.calendar.Gregorian;
 
 
 public class ValidateCliente {
@@ -80,18 +83,21 @@ public class ValidateCliente {
             //erro += "\nDescrição vazia.";
             errors.put(Cliente.EMAIL + "_empty", "Campo email vazio.");
         }
-        if (cliente.getTelefone()== null || cliente.getTelefone().equals("")) {
+        if (cliente.getTelefone()== null || cliente.getTelefone().equals("") || cliente.getTelefone().length() <= 14 || cliente.getTelefone().length() >= 8) {
             //erro += "\nDescrição vazia.";
             errors.put(Cliente.TELEFONE + "_empty", "Campo telefone vazio.");
         }
-        if (cliente.getCpf()== null || cliente.getCpf().equals("")) {
+        if (cliente.getCpf()== null || cliente.getCpf().equals("") || cliente.getCpf().length() == 14) {
             //erro += "\nDescrição vazia.";
             errors.put(Cliente.CPF + "_empty", "Campo cpf vazio.");
         }
-        if (cliente.getDataNascimento()== null) {
+        
+        GregorianCalendar data = new GregorianCalendar();   
+        if (cliente.getDataNascimento()== null || data.get(Calendar.YEAR) - cliente.getDataNascimento().get(Calendar.YEAR) <= 120 ) {
             //erro += "\nDescrição vazia.";
             errors.put(Cliente.DATA_NASCIMENTO + "_empty", "Campo data de nascimento vazio.");
         }
+        
         if (cliente.getPets()== null) {
             //erro += "\nDescrição vazia.";
             errors.put(Cliente.PETS + "_empty", "Campo data de nascimento vazio.");
